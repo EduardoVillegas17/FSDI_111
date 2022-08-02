788da989cce175092193b2eb9dd3e5ebbf3aab13
Luis Eduardo <luis.villegas17@tectijuana.edu.mx>
2022-08-02 14:25:18 -0700

    Created empty files

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from flask import Flask #we imported Flask class from flask module
+
+app = Flask(__name__) #app is an instance
+
+@app.route("/") #this is a route
+def index(): #our view function
+    return "<h1>Hello word</h1>" # return string
+
+@app.route("/about")
+def about():
+    me={
+        "first_name":"Eduardo",
+        "second_name":"Villegas"
+    }
+    return me
+
+
