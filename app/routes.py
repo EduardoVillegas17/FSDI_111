83615ebfc8c8235eb66a80e1ff03fb21c9c870f4
Eduardo Villegas <luis.villegas17@tectijuana.edu.mx>
2022-08-02 20:40:08 -0700

    class3

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from flask import Flask #we imported Flask class from flask module
 
 app = Flask(__name__) #app is an instance
 
+
+
 @app.route("/") #this is a route
 def index(): #our view function
     return "<h1>Hello word</h1>" # return string ok
@@ -10,7 +12,8 @@ def index(): #our view function
 def about():
     me={
         "first_name":"Eduardo",
-        "second_name":"Villegas"
+        "second_name":"Villegas",
+        "hobbies": "do sport"
     }
     return me
 
